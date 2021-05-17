d5cb273ed6e380601f294f891a9b2b852797b1e1
Mickael FOSSO <fosso.mickael@gmail.com>
2021-05-17 12:06:40 +0200

    with answer logic and css

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,19 @@
 class QuestionsController < ApplicationController
   def ask
   end
+
+  def answer
+    @question = params[:question]
+    @answer = 'You did not ask a question!'
+    @answer = analyze_question(params[:question]) if params[:question]
+  end
+
+  private
+
+  def analyze_question(question)
+    return 'Great!' if question == 'I am going to work'
+    return 'Silly question, get dressed and go to work!.' if question.include? "?"
+
+    "I don't care, get dressed and go to work!"
+  end
 end
